a0103115f9f230d626730d218ecd4cccfb8f48b6
Martin Sang Do <dothanhsang1984@yahoo.com>
2025-07-02 08:32:29 +0700

    - Finish question 2

---
+++ src/Question1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Question1 {
+    static String findSubString(String originalString){
+        String subString = "";
+
+
+        return subString;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input a string:");
+        String originalString = sc.nextLine();
+        System.out.println("Original string: " + originalString);
+
+
+        sc.close();
+    }
+}
